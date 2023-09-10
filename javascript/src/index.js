55e8a13b248859bdae681a3d3fa172c655b0755c
JPablo <juapabgonzalez@utp.edu.co>
2023-09-09 19:15:43 -0500

    Gifs con javascript

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,51 +1,12 @@
-console.log('hallo welt!')
+const apikey = 'NGv2bSa4V3T9Y6bUqHFhPSw0o4S8p9Nc';
 
-//no se debe usar var
-
-const name1 = 'Juan ';
-const last = 'Gonzalez';
-
-const fullName = name1 + last;
-
-console.log(name1 + last);
-
-const persona = {
-    name: 'Juan',
-    last: 'Gonzalez',
-    age: 25,
-    address: {
-        'city': 'Bogota', 
-        'zip': 12345, 
-        'lat': 14.3232, 
-        'lng': 34.23423
-    }
-}
-
-console.log(persona);
-
-const arreglo = [1,2,3,4];
-
-let arreglo2 = [...arreglo, 5];
-
-const arreglo3 = arreglo2.map(function(number){
-    return number * 2;
-}
-);
-
-console.log(arreglo);
-console.log(arreglo2);
-console.log(arreglo3);
-
-//Ejercicio con funcion de flecha
-
-const getUserActive= (name)=>({
-    uid: 'ABC123',
-    username: name
-});
-
-const userActive = getUserActive('Juan');
-console.log(userActive);
-
-
-
-
+const petcion = fetch('http://api.giphy.com/v1/gifs/random?api_key=' + apikey);
+petcion
+    .then(resp => resp.json())
+    .then(({ data }) => {
+        const { url } = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    })
+    .catch(console.warn);
